30ebbeedfc6d098350a25e91dcac6bdc63e12b63
Martin Albert <martin.albert.187@gmail.com>
2024-04-22 22:03:27 +0200

    feat: prepare CLI

--- cmd/baton-aruba-central/config.go
+++ cmd/baton-aruba-central/config.go
@@ -4,14 +4,49 @@ import (
 	"context"
 
 	"github.com/conductorone/baton-sdk/pkg/cli"
+	"github.com/spf13/cobra"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 // config defines the external configuration required for the connector to run.
 type config struct {
 	cli.BaseConfig `mapstructure:",squash"` // Puts the base config options in the same place as the connector options
+
+	BaseHost          string `mapstructure:"api-base-host"`
+	ArubaClientID     string `mapstructure:"aruba-central-client-id"`
+	ArubaClientSecret string `mapstructure:"aruba-central-client-secret"`
+	AccessToken       string `mapstructure:"access-token"`
+	RefreshToken      string `mapstructure:"refresh-token"`
 }
 
 // validateConfig is run after the configuration is loaded, and should return an error if it isn't valid.
 func validateConfig(ctx context.Context, cfg *config) error {
+	if cfg.ArubaClientID == "" {
+		return status.Errorf(codes.InvalidArgument, "aruba-central-client-id is required, use --help for more information")
+	}
+
+	if cfg.ArubaClientSecret == "" {
+		return status.Errorf(codes.InvalidArgument, "aruba-central-client-secret is required, use --help for more information")
+	}
+
+	if cfg.AccessToken == "" {
+		return status.Errorf(codes.InvalidArgument, "access-token is required, use --help for more information")
+	}
+
+	if cfg.RefreshToken == "" {
+		return status.Errorf(codes.InvalidArgument, "refresh-token is required, use --help for more information")
+	}
+
 	return nil
+}
+
+func cmdFlags(cmd *cobra.Command) {
+	// api base host - default region is US West 5 (more information about other regions:
+	// https://developer.arubanetworks.com/aruba-central/docs/api-oauth-access-token#table-domain-urls-for-api-gateway-access
+	cmd.PersistentFlags().String("api-base-host", "apigw-uswest5.central.arubanetworks.com", "The base hostname for the Aruba Central API. ($BATON_API_BASE_HOST)")
+	cmd.PersistentFlags().String("aruba-central-client-id", "", "The client ID of the OAuth2 application for the Aruba Central API. ($BATON_ARUBA_CENTRAL_CLIENT_ID)")
+	cmd.PersistentFlags().String("aruba-central-client-secret", "", "The client secret of the OAuth2 application for the Aruba Central API. ($BATON_ARUBA_CENTRAL_CLIENT_SECRET)")
+	cmd.PersistentFlags().String("access-token", "", "The access token for the Aruba Central API. ($BATON_ACCESS_TOKEN)")
+	cmd.PersistentFlags().String("refresh-token", "", "The refresh token for the Aruba Central API. ($BATON_REFRESH_TOKEN)")
 }
--- cmd/baton-aruba-central/main.go
+++ cmd/baton-aruba-central/main.go
@@ -3,13 +3,17 @@ package main
 import (
 	"context"
 	"fmt"
+	"net/http"
+	"net/url"
 	"os"
 
 	"github.com/conductorone/baton-sdk/pkg/cli"
 	"github.com/conductorone/baton-sdk/pkg/connectorbuilder"
 	"github.com/conductorone/baton-sdk/pkg/types"
+	"github.com/conductorone/baton-sdk/pkg/uhttp"
 	"github.com/grpc-ecosystem/go-grpc-middleware/logging/zap/ctxzap"
 	"go.uber.org/zap"
+	"golang.org/x/oauth2"
 
 	"github.com/conductorone/baton-aruba-central/pkg/connector"
 )
@@ -27,6 +31,7 @@ func main() {
 	}
 
 	cmd.Version = version
+	cmdFlags(cmd)
 
 	err = cmd.Execute()
 	if err != nil {
@@ -35,10 +40,61 @@ func main() {
 	}
 }
 
+// TODO: use SDK helper when available
+type OAuth2RefreshToken struct {
+	cfg          *oauth2.Config
+	accessToken  string
+	refreshToken string
+}
+
+func NewOAuth2RefreshToken(clientID, clientSecret, redirectURI, tokenURL, accessToken, refreshToken string) *OAuth2RefreshToken {
+	return &OAuth2RefreshToken{
+		cfg: &oauth2.Config{
+			ClientID:     clientID,
+			ClientSecret: clientSecret,
+			Scopes:       []string{"all"},
+			RedirectURL:  redirectURI,
+			Endpoint: oauth2.Endpoint{
+				TokenURL: tokenURL,
+			},
+		},
+		accessToken:  accessToken,
+		refreshToken: refreshToken,
+	}
+}
+
+func (o *OAuth2RefreshToken) GetClient(ctx context.Context, options ...uhttp.Option) (*http.Client, error) {
+	token := &oauth2.Token{
+		AccessToken:  o.accessToken,
+		RefreshToken: o.refreshToken,
+		TokenType:    "Bearer",
+	}
+	httpClient := o.cfg.Client(ctx, token)
+
+	return httpClient, nil
+}
+
+const (
+	RedirectURI = "https://arubanetworks.com"
+)
+
 func getConnector(ctx context.Context, cfg *config) (types.ConnectorServer, error) {
 	l := ctxzap.Extract(ctx)
 
-	cb, err := connector.New(ctx)
+	tokenURL, err := url.JoinPath(cfg.BaseHost, "/oauth2/token")
+	if err != nil {
+		return nil, fmt.Errorf("error creating token URL: %w", err)
+	}
+
+	credentials := NewOAuth2RefreshToken(
+		cfg.ArubaClientID,
+		cfg.ArubaClientSecret,
+		RedirectURI,
+		tokenURL,
+		cfg.AccessToken,
+		cfg.RefreshToken,
+	)
+	cb, err := connector.New(ctx, cfg.BaseHost, credentials)
 	if err != nil {
 		l.Error("error creating connector", zap.Error(err))
 		return nil, err
